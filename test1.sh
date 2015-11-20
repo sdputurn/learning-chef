e91df7d2fe2f5acfaa1c127ea2bd8dbe97e98f55
Sandeep Singh <sansing5@cisco.com>
2015-11-20 13:50:21 +0530

    testing project rename and remotes

--- test1.sh
+++ test1.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 echo "testing git pull"
-echo ""testing git pull for remote branches"
+echo "testing git pull for remote branches"
 echo "testing git pull and tracking branches merge"
 echo "testing git pull and local branch merge"
 echo "chnages only in copy repo. testing multiple remotes"
 echo "testing multiple remotes. chnages from git server"
+echo "testing project rename and remotes"
