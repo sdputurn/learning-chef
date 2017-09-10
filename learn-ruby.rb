cf141ec39c2bcaef4645e9e453012a96a59f04ba
sandeepkumarsingh <sdp.uturn@gmail.com>
2017-09-11 03:27:00 +0530

    added some more examples of ruby programing

--- learn-ruby.rb
+++ learn-ruby.rb
@@ -18,9 +18,16 @@ END {
     this comment1 
     this is comment2
 =end
+VAR=5
+$global_script_no = 1
+# $class_var = 'test'
 class Customer
     @@no_of_customers = 0
-    TOTAL_COUNT = 0
+    TOTAL_COUNT = 10
+    @@class_var = 'test'
+    cons_var = 'constant_var' #this variable can not be referenced any where inside class definations as this is local variable however constants are accesssible
+    Cons_var  = 'constant_var'
+    puts '++++++++++++',cons_var #this pattern printed once does not matter how many instance you create. may be when the first tme class instance is created in memory
     def initialize(id,name,address)
         @name = name
         @id = id
@@ -28,17 +35,34 @@ class Customer
         @@no_of_customers = @@no_of_customers + 1
         # TOTAL_COUNT = TOTAL_COUNT+1
     end
-    def display
-        puts "name - #{@name}"
+    def display(my_var)
+        puts "name - #{@name}, mesg - #{my_var}"
+        new_var  = VAR + 10
+        puts "script lcal variable - #{new_var}"
+        $global_script_no = 2
+        puts " global_script_no - #{$global_script_no}"
+        
     end
     def display_count()
         puts "no of cutomers - #{@@no_of_customers} " #, @@no_of_customers
     end
+    def return_var (my_var = 'test')
+        puts Cons_var
+        puts @@class_var
+        if my_var == @@class_var
+            return @@class_var+' hi'
+        else
+            return my_var
+        end
+    end
+
 end
-o1 = Customer.new(1,'sandeep','ejipura')
-o2 = Customer.new(2,'bob','bangalore')
-o1.display()
-o2.display()
+# o1 = Customer.new(1,'sandeep','ejipura')
+# o2 = Customer.new(2,'bob','bangalore')
+# o1.display 'alice in wonderland'
+# o2.display ('bob in disney')
+# puts '++++++++++++++++++++++'
+# puts o1.return_var 'hello'
 # o1.display_count
 # o2.display_count
 puts Customer::TOTAL_COUNT
@@ -49,9 +73,25 @@ var = 1 #local variable
 @var = 2 #instance variable
 VAR = 3 #constant
 
-puts var, @var, VAR
-puts " #{var},  #@var, #{@var} , #{VAR}"
+VAR = VAR + 1
+print var,' ++ ', @var, VAR,"\n"
+puts " #{var},  #@var, #{@var} , #{VAR}, #VAR"
 
+require 'json'
+
+filename='input.json'
+f=open(filename,'w')
+h={"name" => 'alice'}
+s= JSON.dump(h)
+f.write(s)
+f.close()
+puts '++++++ file writing completed++++++'
+# p '++++++++++++file reading started ++++++++'
+f=open(filename,'r')
+h=JSON.load(f)
+p h
+p h['name']
+p '++++++++++ file reading completed +++++++++++++'
 #A simple sorting program
 
 # arr = [2,3,10,4,17,18,15]
